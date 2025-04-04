7454aada9ea3246cd8b19de5ee0b5bd85e87f706
gaurav0973 <codejourneypanda@gmail.com>
2025-04-04 21:22:04 +0530

    user model is created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 dotenv.config({
     path : "../.env"
 })
 
 const app = express()
 const port = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: "GET, POST, PUT, DELETE",
+}))
 
 
 
---
+++ src/models/user.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpiry: Date,
+    resetPasswordToken: String,
+    resetPasswordTokenExpiry: Date,
+    refreshToken: String,
+  },
+  { timestamps: true }
+);
+
+export default User;
